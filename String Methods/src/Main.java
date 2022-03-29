991152bb49bef2eb01c47224c443082c4d6b6598
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 16:11:24 -0700

    Add files via upload

---
+++ String Methods/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "James";
+		//boolean result = name.equals("James");
+		//int result = name.length();
+		//char result = name.charAt(0);
+		//int result = name.indexOf("s");
+		//boolean result = name.isBlank();
+		String result = name.toUpperCase();
+		
+		System.out.println(result);		
+				
+	}
+
+}
